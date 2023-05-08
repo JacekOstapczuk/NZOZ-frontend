96c059aa14d8f9be6d51ffb4a509790ed55518b8
Jacek Ostapczuk <jacekostapczuk86@gmail.com>
2023-05-08 08:46:46 +0200

    First commit

---
+++ src/main/java/com/nzozfrontend/NzozFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.nzozfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzozFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NzozFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nzozfrontend/NzozFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzozfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzozFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
